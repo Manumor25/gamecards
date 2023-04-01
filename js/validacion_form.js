0b3ebb3cffb522ff33af01526eb613afbd38303d
manuel morales <man.moralesm@duocuc.cl>
2023-04-01 03:41:20 -0300

    segundo commit

--- js/validacion_form.js
+++ js/validacion_form.js
@@ -1,59 +1,27 @@
 $(document).ready(function() {
     $("#contact-form").validate({
       rules: {
-        name : {
+        text : {
           required: true,
           minlength: 3
         },
-        email: {
-          required: true,
-          email: true
-        },
-        fruit: {
-            required: true,
-        },
-        instrument: {
-            required: true,
-        },
-        cmbEdades:{
-            required: true,
-            number: true,
-        },
-        subjet: {
+        
+        pasword: {
             required: true,
             minlength: 3
         },
-        message: {
-            required: true,
-            minlength: 3
-        }
+        
       },
       messages : {
-        name: {
+        text: {
           minlength: "Name should be at least 3 characters"
         },
-        email: {
-            email: "Debe tener sintaxis de email"
-        },
-        fruit: {
-          required: "Por favor seleciona una fruta",
-
-        },
-        instrument: {
-            required: "Ingresa el instrumento",
-        },
-        cmbEdades:{
-            required: "Debe seleccionar edad",
-            number: true,
-        },
-        subjet: {
+        
+        pasword: {
             required: "Ingrese titulo mensaje",
             minlength: "Largo min 3 caracteres"
         },
-        message: {
-            required: "Ingrese texto del mensaje",
-            minlength: "Largo min 3 caracteres"
-        }
+       
       }
     });
   });
